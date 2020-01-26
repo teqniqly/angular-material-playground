dc2caba01e517714c407e57d08aa3dc870048e5c
farooq-teqniqly <49829654+farooq-teqniqly@users.noreply.github.com>
2020-01-25 19:50:06 -0800

    Starter login form (#4)

--- tq/libs/fitness-tracker/auth/src/lib/login/login.component.ts
+++ tq/libs/fitness-tracker/auth/src/lib/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'tq-login',
@@ -6,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  public showLoginError = false;
+
   constructor() {}
 
   ngOnInit() {}
+
+  onSubmit(form: NgForm): void {
+    this.showLoginError = true;
+  }
 }
