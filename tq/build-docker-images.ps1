4a9c0307e81c6b9773c1ce66e8f87933b8319faf
Farooq Mahmud <>
2020-01-25 23:09:42 -0800

    Making sidenav and toolbar responsive

--- tq/build-docker-images.ps1
+++ tq/build-docker-images.ps1
@@ -2,5 +2,5 @@
 [string]$containerName = "fitness-tracker-app"
 
 docker container rm -f $containerName
-docker image build --tag $imageName .
+docker image build --tag $imageName --file fitness-tracker.dockerfile .
 docker container run -p 4900:80 -d --name $containerName $imageName
