0fb620dfdd414abceca5420a3c8f0bb285de3a94
farooq-teqniqly <49829654+farooq-teqniqly@users.noreply.github.com>
2020-01-26 12:04:58 -0800

    Welcome component (#8)

--- tq/libs/fitness-tracker/welcome/src/lib/fitness-tracker-welcome.module.ts
+++ tq/libs/fitness-tracker/welcome/src/lib/fitness-tracker-welcome.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { WelcomeComponent } from './welcome/welcome.component';
-
+import { UiMaterialUiModule } from '@tq/ui/material-ui';
 @NgModule({
   imports: [
     CommonModule,
+    UiMaterialUiModule,
     RouterModule.forChild([
       { path: '', pathMatch: 'full', component: WelcomeComponent }
     ])
