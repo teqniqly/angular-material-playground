8d93eeca7a165a507c792275e0d919117f908654
farooq-teqniqly <49829654+farooq-teqniqly@users.noreply.github.com>
2020-01-25 18:20:57 -0800

    Added Material date picker component to UI library (#3)

---
+++ tq/libs/ui/material-ui/src/lib/widgets/material-ui-date-picker/material-ui-date-picker.component.ts
@@ -0,0 +1,37 @@
+import {
+  Component,
+  OnInit,
+  Input,
+  Output,
+  EventEmitter,
+  ChangeDetectionStrategy
+} from '@angular/core';
+import { MatDatepickerInputEvent } from '@angular/material';
+
+@Component({
+  selector: 'tq-material-ui-date-picker',
+  templateUrl: './material-ui-date-picker.component.html',
+  styleUrls: ['./material-ui-date-picker.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class MaterialUiDatePickerComponent implements OnInit {
+  constructor() {}
+
+  @Input()
+  placeholder = 'Choose a date.';
+
+  @Input()
+  minDate: Date;
+
+  @Input()
+  maxDate: Date;
+
+  @Output()
+  dateChanged: EventEmitter<Date> = new EventEmitter<Date>();
+
+  ngOnInit() {}
+
+  onDateChanged(event: MatDatepickerInputEvent<Date>): void {
+    this.dateChanged.emit(event.value);
+  }
+}
